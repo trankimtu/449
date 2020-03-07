44a94487e29aedc4e6f3ffba586bfa953a724679
trankimtu <trankimtu@gmail.com>
2020-03-06 21:58:36 -0800

    Finish before testing

--- test.py
+++ test.py
@@ -1,8 +1,23 @@
 import requests
 
-resp = requests.get('https://todolist.example.com/tasks/')
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-for todo_item in resp.json():
-    print('{} {}'.format(todo_item['id'], todo_item['summary']))
+# test get all data
+def test_Get_All():
+
+
+    resp = requests.get('http://127.0.0.1:5000/posts/all')
+    print(resp.status_code)
+    if resp.status_code == 200:
+
+        print ('Get All Data works well!')
+        obj = resp.json()
+        length = len(obj)
+        print(obj)
+        print(f'The list has {length} items.')
+    else:
+        print ('Get All Data DOES NOT works well!')
+
+        # raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+    # for todo_item in resp.json():
+    #     print('{} {}'.format(todo_item['id'], todo_item['summary']))
+
+test_Get_All()
