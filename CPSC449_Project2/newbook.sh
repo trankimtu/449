2982942c800a3336b48852ee998c4955809db07c
trankimtu <33793431+trankimtu@users.noreply.github.com>
2020-04-29 14:21:55 -0700

    Delete newbook.sh

--- CPSC449_Project2/newbook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-curl --verbose \
-     --request POST \
-     --header 'Content-Type: application/json' \
-     --data @newvote.json \
-    http://localhost:5000/api/v1/resources/votes
-
